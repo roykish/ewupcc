efe9b3ea355565569920b3f7c57bcaf3c8291829
Kishor Roy <roykishor93@gmail.com>
2022-07-31 10:26:56 +0600

    first commit

---
+++ src/app/home-page/home-page-content/home-page-content.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomePageContentComponent } from './home-page-content.component';
+
+describe('HomePageContentComponent', () => {
+  let component: HomePageContentComponent;
+  let fixture: ComponentFixture<HomePageContentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HomePageContentComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HomePageContentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
