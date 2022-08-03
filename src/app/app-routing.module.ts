9702e3d41adfc9bf9ee20c117c1461112d7581a0
Kishor Roy <roykishor93@gmail.com>
2022-08-03 10:33:17 +0600

    shifter routes from app.module to routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsPageComponent } from './about-us-page/about-us-page.component';
+import { DonatePageComponent } from './donate-page/donate-page.component';
+import { GalleryPageComponent } from './gallery-page/gallery-page.component';
+import { HomePageContentComponent } from './home-page/home-page-content/home-page-content.component';
+import { AlumniPageComponent } from './members-page/alumni-page/alumni-page.component';
+import { CurrentCommitteePageComponent } from './members-page/current-committee-page/current-committee-page.component';
+import { GeneralMembersPageComponent } from './members-page/general-members-page/general-members-page.component';
+import { MembersPageComponent } from './members-page/members-page.component';
 
-const routes: Routes = [];
+const appRoutes : Routes = [
+  {path: '', component: HomePageContentComponent},
+  {path: 'gallery', component: GalleryPageComponent},
+  {path: 'about-us', component: AboutUsPageComponent},
+  {path: 'members', component: MembersPageComponent,
+    children:
+      [
+        {path: 'current-committe', component: CurrentCommitteePageComponent},
+        {path: 'general-members', component: GeneralMembersPageComponent},
+        {path: 'alumni', component: AlumniPageComponent}
+      ]
+  },
+  {path: 'donate', component: DonatePageComponent}
+
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,33 +11,12 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { HomePageContentComponent } from './home-page/home-page-content/home-page-content.component';
 import { GalleryPageComponent } from './gallery-page/gallery-page.component';
 import { AboutUsPageComponent } from './about-us-page/about-us-page.component';
-import{ RouterModule, Routes } from '@angular/router';
+
 import { MembersPageComponent } from './members-page/members-page.component';
 import { DonatePageComponent } from './donate-page/donate-page.component';
 import { CurrentCommitteePageComponent } from './members-page/current-committee-page/current-committee-page.component';
 import { GeneralMembersPageComponent } from './members-page/general-members-page/general-members-page.component';
 import { AlumniPageComponent } from './members-page/alumni-page/alumni-page.component';
-
-
-
-
-const appRoutes : Routes = [
-  {path: '', component: HomePageContentComponent},
-  {path: 'gallery', component: GalleryPageComponent},
-  {path: 'about-us', component: AboutUsPageComponent},
-  {path: 'members', component: MembersPageComponent,
-    children:
-      [
-        {path: 'current-committe', component: CurrentCommitteePageComponent},
-        {path: 'general-members', component: GeneralMembersPageComponent},
-        {path: 'alumni', component: AlumniPageComponent}
-      ]
-
-  },
-  {path: 'donate', component: DonatePageComponent},
-
-]
-
 
 @NgModule({
   declarations: [
@@ -52,13 +31,13 @@ const appRoutes : Routes = [
     CurrentCommitteePageComponent,
     GeneralMembersPageComponent,
     AlumniPageComponent,
-
+    AboutUsPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(appRoutes),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
